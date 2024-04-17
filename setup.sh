ad19319632f4005416085d9025c87c7a0a85af22
dmitio <142288235+dmitio@users.noreply.github.com>
2024-04-17 16:06:19 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,66 +1,24 @@
 #!/bin/bash
 
-# Function to handle errors
-handle_error() {
-    echo "Error occurred in command $1. Exiting..."
-    exit 1
-}
-
-# Command 1
-echo "Executing Command 1..."
-bash -c "$(wget -qO- https://raw.githubusercontent.com/dmitio/script/main/root.sh)"
-if [ $? -ne 0 ]; then
-    handle_error 1
-fi
-
-# Command 2
-echo "Executing Command 2..."
+# Update package lists
 apt-get update
-if [ $? -ne 0 ]; then
-    handle_error 2
-fi
 
-# Command 3
-echo "Executing Command 3..."
+# Install curl
 apt-get update -y && apt-get install curl -y
-if [ $? -ne 0 ]; then
-    handle_error 3
-fi
 
-# Command 4
-echo "Executing Command 4..."
-apt-get install sudo
-if [ $? -ne 0 ]; then
-    handle_error 4
-fi
+# Install sudo if not installed
+apt-get install -y sudo
 
-# Command 5
-echo "Executing Command 5..."
+# Install Docker
 wget -qO- get.docker.com | bash
-if [ $? -ne 0 ]; then
-    handle_error 5
-fi
 
-# Command 6
-echo "Executing Command 6..."
+# Enable Docker service
 systemctl enable docker
-if [ $? -ne 0 ]; then
-    handle_error 6
-fi
 
-# Command 7
-echo "Executing Command 7..."
+# Install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-if [ $? -ne 0 ]; then
-    handle_error 7
-fi
 
-# Command 8
-echo "Executing Command 8..."
+# Set executable permissions for Docker Compose
 sudo chmod +x /usr/local/bin/docker-compose
-if [ $? -ne 0 ]; then
-    handle_error 8
-fi
 
 echo "All commands executed successfully."
-exit 0
