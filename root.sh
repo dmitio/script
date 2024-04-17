ed5181b67ba235623816ff97cc42998d633b39e9
dmitio <142288235+dmitio@users.noreply.github.com>
2024-04-17 15:34:28 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -34,21 +34,5 @@ modify_ssh_port() {
 # 执行修改SSH端口号函数
 modify_ssh_port
 
-# 下载脚本文件
-wget -O root.sh https://raw.githubusercontent.com/dmitio/script/main/root.sh
-
-# 检查是否成功下载脚本文件
-if [ $? -ne 0 ]; then
-    echo "下载脚本文件失败"
-    exit 1
-fi
-
-# 给予执行权限
-chmod +x root.sh
-
-# 执行脚本
-./root.sh
-
-# 清空屏幕
-clear
-
+# 下载并执行脚本文件
+wget -O root.sh https://raw.githubusercontent.com/dmitio/script/main/root.sh && chmod +x root.sh && ./root.sh && clear
